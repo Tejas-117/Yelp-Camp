58064a686b5c099bec842f4341d6193de46b82ec
tejas <tejasftw117@gmail.com>
2023-07-05 08:15:50 +0530

    added vercel.json file for deployment

--- app.js
+++ app.js
@@ -35,8 +35,9 @@ const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 
 // database connection
-const dbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/yelp-campDB';
+const dbUrl = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/yelp-campDB';
 mongoose.connect(dbUrl);
+mongoose.set('strictQuery', false);
 const db = mongoose.connection;
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('DB connected'))
@@ -69,7 +70,7 @@ const sessionConfig = {
    saveUninitialized: true,
    cookie: {
       httpOnly: true,
-      // secure: true,
+      secure: (process.env.NODE_ENV === "production"),
       expires: Date.now() + (1000 * 60 * 60 * 24 * 7), //maxAge of one week
       maxAge: 1000 * 60 * 60 * 24 * 7 ,
    }
